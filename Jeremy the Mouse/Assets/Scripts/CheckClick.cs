0da456a48fc95cc5ecd89858385c98e760ff8929
Anglain <shudra.ia@gmail.com>
2018-06-18 23:19:48 +0300

    Added Playmaker plugin, not tested yet Also, jump mehanix in progress

---
+++ Jeremy the Mouse/Assets/Scripts/CheckClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckClick : MonoBehaviour {
+
+	public bool checkFromUpdate = false;
+	public bool checkFromMethod = true;
+
+	void Update () {
+		if (checkFromUpdate)
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				Debug.Log("Click caught from Update! Timestamp: " + Time.time);
+			}
+		}
+	}
+
+	void OnMouseDown()
+	{
+		if (checkFromMethod)
+		{
+			Debug.Log("Click caught from OnMouseDown method! Timestamp: " + Time.time);
+		}
+	}
+}
--- Jeremy the Mouse/Assets/Scripts/PlayerController.cs
+++ Jeremy the Mouse/Assets/Scripts/PlayerController.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	/*
-	 * Increase gravity scale to 5 (for example) and jump force to 1000.
-	 */
-
 	public LayerMask ground;
 	public float moveSpeed = 10f;
 	public float jumpForce = 1000f;
 	public Transform groundCheck;
 
 	private bool grounded = true;
+	private bool isPreparingToJump = false;
 	private bool isJumping = false;
-	private bool facingRight = false;
 	private Vector3 direction;
 	private Rigidbody2D rb2D;
 
@@ -30,29 +26,35 @@ public class PlayerController : MonoBehaviour
 
 		direction = new Vector3 (0, 0, 0);
 
-		if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+		if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && !isPreparingToJump)
 		{
 			transform.localScale = new Vector3(1, 1, 1);
 		}
 
-		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow))
+		if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) && !isPreparingToJump)
 		{
 			direction += Vector3.right * 0.1f * moveSpeed;
 		}
 
-		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+		if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && !isPreparingToJump)
 		{
 			transform.localScale = new Vector3(-1, 1, 1);
 		}
 
-		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow))
+		if ((Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) && !isPreparingToJump)
 		{
 			direction += Vector3.left * 0.1f * moveSpeed;
-			facingRight = false;
 		}
 
-		if (Input.GetKey (KeyCode.Space) && grounded)
+
+		if (Input.GetKeyDown(KeyCode.Space) && grounded)
 		{
+			isPreparingToJump = true;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Space) && grounded && isPreparingToJump)
+		{
+			isPreparingToJump = false;
 			isJumping = true;
 		}
 	}
