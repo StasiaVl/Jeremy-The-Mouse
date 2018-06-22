314fb076a63ff1e844dc5bfd7ea5344a6ab1a028
Anglain <shudra.ia@gmail.com>
2018-06-23 02:30:45 +0300

    Rope mehanix in progress

---
+++ Jeremy the Mouse/Assets/Scripts/HangingPlayerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangingPlayerTest : MonoBehaviour {
+
+	public float hangRotatePower = 100f;
+
+	private Rigidbody2D rb2D;
+
+	void Awake ()
+	{
+		rb2D = GetComponent<Rigidbody2D> ();
+
+		if (rb2D == null) Debug.LogError("No Rigidbody2D component found assigned to this gameObject! [HANGING_PLAYER_TEST.CS]");
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			rb2D.AddTorque(-hangRotatePower);
+		} else if (Input.GetKeyDown(KeyCode.D))
+		{
+			rb2D.AddTorque(hangRotatePower);
+		}
+	}
+}
